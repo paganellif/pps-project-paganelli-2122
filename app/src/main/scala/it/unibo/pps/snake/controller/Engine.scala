07d3b1dbb52e0f045090d5e9926242c2b7a5bf5f
paganellif <filippo1paganelli@gmail.com>
2022-01-01 22:45:02 +0100

    fix logging

--- app/src/main/scala/it/unibo/pps/snake/controller/Engine.scala
+++ app/src/main/scala/it/unibo/pps/snake/controller/Engine.scala
@@ -2,9 +2,9 @@ package it.unibo.pps.snake.controller
 
 import io.github.sodium.{Cell, StreamSink}
 import it.unibo.pps.snake.controller.Status.Status
+import org.slf4j.{Logger, LoggerFactory}
 
 import java.lang.Thread.sleep
-import scala.util.control.Breaks.break
 
 object Status extends Enumeration {
   type Status = Value
@@ -15,17 +15,20 @@ case class Event()
 
 case class Engine(isRunning: Cell[Status], speed: Cell[Int]) extends Runnable {
 
+  private val logger: Logger = LoggerFactory.getLogger(Engine.getClass)
   val ticker: StreamSink[Event] = new StreamSink[Event]()
 
   override def run(): Unit = {
     while(true){
       isRunning.sample() match {
-        case Status.RUNNING => ticker.send(Event())
+        case Status.RUNNING => logger.debug("RUNNING STATUS"); ticker.send(Event())
         case Status.STOP => System.exit(0)
-        case Status.PAUSE => println("PAUSE STATUS")
-        case _ => println("Unknown Status")
+        case Status.PAUSE => logger.debug("PAUSE STATUS")
+        case _ => logger.error("Unknown Status")
       }
-      sleep(700 / speed.sample())
+      val freq: Int = (700 / speed.sample()).round
+      logger.debug(s"engine freq: ${freq}")
+      sleep(freq)
     }
   }
 }
--- app/src/main/scala/it/unibo/pps/snake/controller/SnakeController.scala
+++ app/src/main/scala/it/unibo/pps/snake/controller/SnakeController.scala
@@ -1,6 +1,6 @@
 package it.unibo.pps.snake.controller
 
-import io.github.sodium.{Cell, CellSink, Stream}
+import io.github.sodium.{Cell, Stream}
 import it.unibo.pps.snake.model.Directions.Direction
 import it.unibo.pps.snake.model.Food.Score
 import it.unibo.pps.snake.model.World.Boundary
@@ -8,7 +8,6 @@ import it.unibo.pps.snake.model.{Directions, Food, Snake}
 import org.slf4j.LoggerFactory
 
 import java.util.concurrent.{ExecutorService, Executors}
-import scala.collection.mutable.ArrayBuffer
 
 case class SnakeController(
   directionInput: Cell[Directions.Direction],
@@ -18,26 +17,19 @@ case class SnakeController(
 ) {
   private val logger = LoggerFactory.getLogger(SnakeController.getClass)
 
-  /**
-   *
-   */
   private val initSnake: Snake = Snake(Array(((boundary._2/2).round,(boundary._4/2).round)))
-  private val initFood: Array[Food] = Food.createRandomHealthyFoods(Array(),boundary,30)
+  private val initFood: Array[Food] = Food.createRandomFoods(Array(),boundary,5)
 
-  /**
-   *
-   */
-  private val executor: ExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors + 1)
+  private val threadPoolSize: Int = Runtime.getRuntime.availableProcessors + 1
+  private val executor: ExecutorService = Executors.newFixedThreadPool(threadPoolSize)
   private val engine: Engine = Engine(statusInput, speedInput)
 
-  /**
-   *
-   */
   def start(): Unit = executor.execute(engine)
 
   /**
+   * The foods and the snake of the game: modified when the engine ticks.
    *
-   * @return
+   * @return <code>Stream[(Snake,Array[Food])]</code>
    */
   def output: Stream[(Snake,Array[Food])] = engine.ticker
     .accum((initSnake, initFood), (event: Event, acc: (Snake, Array[Food])) => {
@@ -49,7 +41,7 @@ case class SnakeController(
         val s: Snake = acc._1.increase(tmpD).getOrElse(acc._1)
 
         val tmpF: Array[Food] = acc._2.filter(elem => !s.isNearTo(elem.position))
-        val f: Array[Food] = tmpF.prependedAll(Food.createRandomHealthyFoods(
+        val f: Array[Food] = tmpF.prependedAll(Food.createRandomFoods(
             tmpF.map(f => f.position).prependedAll(s.body), boundary, 1))
         (s,f)
       } else {
@@ -59,42 +51,51 @@ case class SnakeController(
   }).updates()
 
   /**
+   * The foods of the game: modified when the snake eats a food.
    *
-   * @return
+   * @return <code>Stream[Array[Food]]</code>
    */
   def foodOutput: Stream[Array[Food]] = output.map(sf => sf._2)
 
   /**
+   * The score of the game: modified when the snake eats a food.
    *
-   * @return
+   * @return <code>Stream[Score]</code>
    */
   def scoreOutput: Stream[Score] = foodOutput.accum((initFood, 0), (fp: Array[Food], acc: (Array[Food],Score)) => {
     val foodDiff = fp.diff(acc._1)
+    logger.debug(s"previous: ${fp.mkString("Array(", ", ", ")")} - " +
+      s"acc: ${acc._1.mkString("Array(", ", ", ")")} - " +
+      s"food diff: ${foodDiff.mkString("Array(", ", ", ")")}")
     if(foodDiff.length > 0){
-      println((fp,acc._2 + foodDiff.head.score))
-      // TODO: fix algo -> aumenta i punti una volta si e una no
-      (fp,acc._2 + foodDiff.head.score)
+      val out = (fp,acc._2 + foodDiff.head.score)
+      logger.debug(s"${foodDiff.head}")
+      logger.debug(s"score output: ${out}")
+      // TODO: fix algo
+      out
     } else {
       (fp,acc._2)
     }
   }).updates().map((fs: (_, Score)) => fs._2)
 
   /**
+   * The snake of the game: modified when the snake is moved or increased.
    *
-   * @return
+   * @return <code>Stream[Snake]</code>
    */
   def snakeOutput: Stream[Snake] = output.map(sf => sf._1)
 
   /**
+   * The snake of the game: modified when the snake is knotted.
    *
-   * @return
+   * @return <code>Stream[Snake]</code>
    */
   def isKnotted: Stream[Snake] = output.filter(s => s._1.isKnotted).map(o => o._1)
 
   /**
+   * The size of the snake: modified when the snake is moved or increased.
    *
-   * @return
+   * @return <code>Stream[Int]</code>
    */
   def snakeSize(): Stream[Int] = output.map(s => s._1.body.length)
-
 }
