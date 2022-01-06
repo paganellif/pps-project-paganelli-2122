73658c68e8c0752176f336dbab155db2f73e3006
paganellif <filippo1paganelli@gmail.com>
2022-01-06 23:36:59 +0100

    refactor snakecontroller fields

--- app/src/main/scala/it/unibo/pps/snake/controller/Engine.scala
+++ app/src/main/scala/it/unibo/pps/snake/controller/Engine.scala
@@ -1,6 +1,6 @@
 package it.unibo.pps.snake.controller
 
-import io.github.sodium.{Cell, StreamSink}
+import io.github.sodium.{CellSink, StreamSink}
 import it.unibo.pps.snake.controller.Status.Status
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -13,20 +13,20 @@ object Status extends Enumeration {
 
 case class Event()
 
-case class Engine(isRunning: Cell[Status], speed: Cell[Int]) extends Runnable {
+case class Engine(statusInput: CellSink[Status], speedInput: CellSink[Int]) extends Runnable {
 
   private val logger: Logger = LoggerFactory.getLogger(Engine.getClass)
   val ticker: StreamSink[Event] = new StreamSink[Event]()
 
   override def run(): Unit = {
     while(true){
-      isRunning.sample() match {
+      statusInput.sample() match {
         case Status.RUNNING => logger.debug("RUNNING STATUS"); ticker.send(Event())
         case Status.STOP => System.exit(0)
         case Status.PAUSE => logger.debug("PAUSE STATUS")
         case _ => logger.error("Unknown Status")
       }
-      val freq: Int = (1000 / speed.sample()).round
+      val freq: Int = (1000 / speedInput.sample()).round
       logger.debug(s"engine freq: ${freq}")
       sleep(freq)
     }
